97eff62bc8d9ea066e2aa8ca38fc48b8cf9eaa0a
Mohammed Taitai <mohammedtaitai@outlook.com>
2022-07-13 20:58:25 -0400

    add 2 event listeners, center styling, added fetch

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-fetch("https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast")
+document.addEventListener("DOMContentLoaded",function() {
+const submitB= document.getElementById('sButton');
+let ingredient= document.getElementById('searchInput');
+
+submitB.addEventListener('click', recipe(ingredient.value))
+
+function recipe(ing){
+    console.log(ing);
+fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ing}`)
   .then(response => response.json())
   .then(data => console.log(data));
+}
 
+});
