094662f5e9c57c391c77308e159e2557c16e6376
Mohammed Taitai <mohammedtaitai@outlook.com>
2022-07-15 13:23:08 -0400

    Added a div area in html for the recipe to go after you hove over it

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+//DOM contentloaded EV1
 document.addEventListener("DOMContentLoaded",function() {
 const submitB= document.getElementById('sButton');
 const foodL= document.getElementById('foodList');
 let ingredient= document.getElementById('searchInput');
+const recpInfo=document.getElementById('mealRecipe')
 
+//Submit button EV2
 submitB.addEventListener('click', recipeL)
+// Mouseover for recipe EV3
+foodL.addEventListener('mouseover', getRecpie)
 
+//GET Data From Meal DB
 function recipeL(){
     console.log(ingredient.value);
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient.value}`)
@@ -19,14 +25,47 @@ function recipeL(){
 });
 }
 
+//Renders data into list
 function renderRCards(data){
     console.log(data);
     let card = document.createElement('li')
     card.className='cards'
     card.innerHTML =`
+    <div id=${data.idMeal}>
     <img src = "${data.strMealThumb}">
     <h2> ${data.strMeal}</h2>
+    <a href = "#" class = "recipe-btn">Get Recipe</a>
+    </div>
     `
     foodL.appendChild(card);
 }
+
+
+function getRecpie(e){
+    e.preventDefault();
+    if(e.target.classList.contains('recipe-btn')){
+        let recp = e.target.parentElement;
+        console.log(recp)
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recp.id}`)
+        .then(response=>response.json())
+        .then(data=>{recipeInformation(data)})
+
+    }
+
+}
+function recipeInformation(data){
+    recpInfo.innerHTML=""
+    recpObj=data.meals[0];
+    console.log(recpObj.strMeal)
+    let recipeCard =`
+    <h3>${recpObj.strMeal}</h3>
+    <img src = "${recpObj.strMealThumb}">
+    <div>
+        <h4>Instructions</h4>
+        <p> ${recpObj.strInstructions}</p>
+    </div>
+    `
+    recpInfo.innerHTML=recipeCard;
+}
+
 });
